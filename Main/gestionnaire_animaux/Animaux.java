379d9de025e0984c523f7c74205a61641cecbd2d
Domi <domitillevale@gmail.com>
2024-11-22 09:36:03 +0100

    ajout age et genre pour les animaux

--- Main/gestionnaire_animaux/Animaux.java
+++ Main/gestionnaire_animaux/Animaux.java
@@ -2,5 +2,24 @@ package gestionnaire_animaux;
 
 public abstract class Animaux{
 
+    protected int genre; // 0 : femelle / 1 : male / 2 : autre
+    protected int age; // en saison
+
+    public void setAge(int n){
+        age=n;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setGenre(int n){
+        genre=n;
+    }
+
+    public int getGenre(){
+        return genre;
+    }
+
     public abstract void GererSaison();
 }
